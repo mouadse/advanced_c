c723ff90e6790ac86399b4f54cee61ea7e0d5bd8
mouadse <mouadse@gmail.com>
2024-06-09 22:52:16 +0100

    X

--- functions.c
+++ functions.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -7,13 +8,26 @@ int calc(int a, int b) {
   return (ans);
 }
 
-// the function type of calc is int *(int , int);
+bool p(int num) { return (num % 2 == 0); }
+
+void print_if(int xs[10], bool (*predicate)(int)) {
+  int i = 0;
+
+  while (i < 10) {
+    if (predicate(xs[i]))
+      printf("%d\n", xs[i]);
+    i++;
+  }
+}
 
 int main(void) {
 
-  int (*f)(int, int) = calc;
+  // int (*f)(int, int) = calc;
+  // printf("%d\n", f(12, 10));
 
-  printf("%d\n", f(12, 10));
+  int *numbers = malloc(sizeof(int) * 10);
+  if (!numbers)
+    return (perror("Malloc Failed!!!"), EXIT_FAILURE);
 
   return (EXIT_SUCCESS);
 }
