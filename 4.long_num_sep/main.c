b82468c02bf4a8f7a0ee7acdbcc857e134d51f50
mouadse <mouadse@gmail.com>
2024-06-10 03:42:16 +0100

    More shit incoming

--- 4.long_num_sep/main.c
+++ 4.long_num_sep/main.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+/*
+
+// This code refused to compile multiple times so let's just forget about for
+now
+
+int main() {
+    int large_number = 1'000'000;
+    printf("%d\n", large_number);
+    return 0;
+}
+*/
+
 int main() {
   int large_number = 100 /* thousand */ * 1000 /* thousand */;
   printf("%d\n", large_number);
