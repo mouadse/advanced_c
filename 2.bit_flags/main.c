f7460950e29ba219850038210eff70a53fd3be40
mouadse <mouadse@gmail.com>
2024-06-10 01:19:12 +0100

    bit flags added to mem palace

--- 2.bit_flags/main.c
+++ 2.bit_flags/main.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 
-void printer(char *str) {
-  if (!str)
-    return;
-  write(STDOUT_FILENO, str, strlen(str));
+typedef enum { FLAG_A = (1 << 0), FLAG_B = (1 << 1), FLAG_C = (1 << 2) } t_flag;
+
+void f(int x, t_flag flags) {
+  if (flags & FLAG_A) {
+    printf("Flag A detected !!!\n");
+  }
+
+  if (flags & FLAG_B) {
+    printf("Flag B detected !!!\n");
+  }
+
+  if (flags & FLAG_C) {
+    printf("Flag C detected !!!\n");
+  }
+  (void)x;
 }
 
 int main(void) {
-  char *name = NULL;
-  printer(name);
+  f(1337, FLAG_C | FLAG_A | FLAG_B);
   return (EXIT_SUCCESS);
 }
